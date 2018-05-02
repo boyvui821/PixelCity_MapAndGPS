b718a8e840ca0729638c5ed94677ddf0adc90a35
Nguyen Hieu Trung <ktpm11520669@gmail.com>
2018-05-03 00:46:21 +0700

    Initial Commit

---
+++ PixelCity_MapAndGPS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PixelCity_MapAndGPS
+//
+//  Created by Nguyen Hieu Trung on 5/3/18.
+//  Copyright © 2018 NHTSOFT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
